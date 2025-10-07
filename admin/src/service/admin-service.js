6a76635e93aaff207e3dbfee0ec4a432e36401e4
dangvu <148321391+dangvu2910@users.noreply.github.com>
2025-10-07 22:21:11 +0700

    update service admin

---
+++ admin/src/service/admin-service.js
@@ -0,0 +1,31 @@
+const config = require("../../../../../../Downloads/CSE702025-N06-Nhom-1/CSE702025-N06-Nhom-1/admin/src/config/config");
+const UserModels = require("../../../../../../Downloads/CSE702025-N06-Nhom-1/CSE702025-N06-Nhom-1/admin/src/models/user-models");
+const bcrypt = require("bcrypt");
+
+class AdminService {
+  async generate() {
+    const adminAccount = config.admin.account;
+    const adminPassword = config.admin.password;
+    const admin = await UserModels.findOne({ account: adminAccount });
+    if (admin && admin.role != "Admin") {
+      admin.role = "Admin";
+      await admin.save();
+      return;
+    }
+    if (admin) {
+      return;
+    }
+    const hashedPassword = await bcrypt.hash(adminPassword, 10);
+    const user = new UserModels({});
+    user.account = adminAccount;
+    user.password = hashedPassword;
+    user.role = "Admin";
+    user.name = "Lê Đức Long";
+    user.phone = "0368843600";
+    user.gmail = "longg56789@gmail.com";
+    user.address = "Trên núi";
+    await user.save();
+  }
+}
+
+module.exports = new AdminService();
